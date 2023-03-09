380ba059237954d1c1a63ff090d3ca91e6a8817e
Ngo Quoc Dat <datlechin@gmail.com>
2023-03-09 20:52:57 +0700

    update

--- Assignment/resources/js/app.js
+++ Assignment/resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import 'angular/angular'
+
+const app = angular.module('app', [])
+
+app.config(($interpolateProvider) => {
+    $interpolateProvider.startSymbol('%')
+    $interpolateProvider.endSymbol('%')
+})
+
+app.controller('GreetingController', ($scope) => {
+    $scope.greeting = 'Hello world'
+})
