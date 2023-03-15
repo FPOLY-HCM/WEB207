e94c485bde3fb405c3fbe7bb29dbadb03588eca5
Ngo Quoc Dat <datlechin@gmail.com>
2023-03-15 23:49:03 +0700

    Models

---
+++ Assignment/app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'discussion_id',
+        'content',
+        'ip_address',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function discussion(): BelongsTo
+    {
+        return $this->belongsTo(Discussion::class);
+    }
+}
